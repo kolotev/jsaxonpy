f7d2a56a2fc4f9fd28ecdd2a5aa8a7bab27b69f2
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2022-11-21 19:00:07 -0500

    initial commit

---
+++ src/jsaxonpy/__init__.py
@@ -0,0 +1,10 @@
+"""
+    jsaxonpy - the python package to be used for your Java Saxon XSLT
+    transformations in your python applications.
+"""
+
+from .xslt import Xslt
+
+__version__ = "0.0.0"
+__revision__ = ""
+__all__ = [Xslt]
