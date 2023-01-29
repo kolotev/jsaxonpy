d6daa201630cc5c837c70cf45a15c4c0ed735934
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2023-01-28 19:20:02 -0500

    jvm.py: fixed a failure when no JVM_OPTIONS env variable set/provided.

--- src/jsaxonpy/jvm.py
+++ src/jsaxonpy/jvm.py
@@ -1,18 +1,17 @@
 import os
 
-from .signleton import Singleton
+from .signleton import ProcessSingletonMeta
 
 
-class JVM(metaclass=Singleton):
+class JVM(metaclass=ProcessSingletonMeta):
     def __init__(self):
-        jvm_options = os.getenv("JVM_OPTIONS", "").split(" ")
-        if jvm_options:
-            import jnius_config
+        import jnius_config
 
-            if not jnius_config.vm_running:
-                jnius_config.add_options(*(jvm_options))
-                # jnius_config.add_options("-Xrs", "-Xmx512m", "-XX:ActiveProcessorCount=4")
-                # jnius_config.set_classpath(".", "/pmc/local/Saxon-J/saxon-he-11.4.jar")
+        jvm_options = os.getenv("JVM_OPTIONS", None)
+        if not jnius_config.vm_running and jvm_options:
+            jnius_config.add_options(*(jvm_options.split(" ")))
+            # jnius_config.add_options("-Xmx2048m", "-XX:ActiveProcessorCount=4")
+            # jnius_config.set_classpath(".", "/pmc/local/Saxon-J/saxon-he-11.4.jar")
 
         import jnius
 
