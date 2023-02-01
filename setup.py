7585fc39517a0b6f7cf55ffab5264e722fb53b4e
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2023-02-01 00:31:15 -0500

    PMC-52423:  setup.py: setuptools_scm & setuptools requirements were added;

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup(setup_requires=['tox-setuptools'], tests_require=['tox>=3.3,<4'])
+setup(
+    setup_requires=[
+        "setuptools>=61",
+        "setuptools_scm[toml]>=6.2",
+        "tox-setuptools",
+    ],
+    tests_require=["tox>=3.3,<4"],
+)
