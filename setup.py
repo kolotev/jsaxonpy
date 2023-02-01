94ecab0ba7fd40930aeb2a8113e190b0d68635c6
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2023-02-01 01:23:01 -0500

    PMC-52423:  setup.py:  name, maintainer, maintainer_email were added;

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(
+    name="jsaxonpy",
+    maintainer="Andrei Kolotev",
+    maintainer_email="kolotev@ncbi.nlm.nih.gov",
     setup_requires=[
         "setuptools_scm[toml]>=6.2",
         "tox-setuptools",
